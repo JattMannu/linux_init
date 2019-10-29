6d9b6a816ce45cfbefe0d9b50fa112e1fe24dbc0
Manpreet Singh <manpreet_firewall@yahoo.com.sg>
2019-10-30 00:01:31 +0800

    \ to run all and sample code

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#ssudo apt-get install -y wmctrl
+# sudo apt-get install -y wmctrl
 
 #wmctrl -a chromium
 
@@ -24,4 +24,10 @@ dconf write /org/mate/desktop/keybindings/test0/action "'sh -c ~/t.sh'"
 #dconf write /org/mate/desktop/keybindings/custom0/action "'mate-open'"
 dconf write /org/mate/desktop/keybindings/test0/binding "'<Alt>a'"
 dconf write /org/mate/desktop/keybindings/test0/name "'Run Custom Script'"
+
 echo "done!"
+
+# https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'move-window'"
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Primary><Alt>Page_Down'"
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'/usr/local/bin/move-window.sh'"

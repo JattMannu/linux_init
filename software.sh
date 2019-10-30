499e19705ebd99c5d8cedd05e8c83ef4395e06bb
Manpreet Singh <manpreet.singh@compareasiagroup.com>
2019-10-30 11:44:55 +0800

    Added the software i use
    Signed-off-by: Manpreet Singh <manpreet.singh@compareasiagroup.com>

---
+++ software.sh
@@ -0,0 +1,8 @@
+
+sudo apt-get install -y wmctrl
+sudo apt-get install -y peek
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y nano
+sudo apt-get install -y git 
+sudo apt-get install -y git-gui 
+

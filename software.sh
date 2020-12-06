21b51098d6346bd2728a9a7330d74dfb7f561b5e
JattMannu <manpreet_firewall@yahoo.com.sg>
2020-12-07 00:09:33 +0800

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,4 +1,4 @@
-
+### Chapter 1
 sudo apt-get install -y wmctrl
 #Take gif screenshots
 sudo apt-get install -y peek
@@ -11,16 +11,20 @@ sudo apt-get install -y dconf-tools
 
 
 
+### Chapter 2
 sudo apt-get install -y wmctrl chromium-browser tilix nano git git-gui 
 
 sudo apt-get update
 sudo apt-get upgrade
 
-
+### Chapter 3
 sudo snap install intellij-idea-community --classic
 sudo snap install pycharm-community --classic
 
 
-
+### Chapter 4
 # Automation tool
 sudo apt-get install -y xdotool 
+# Help for postmessage/Sendkeys etc
+sudo apt-get install xserver-xephyr -y
+

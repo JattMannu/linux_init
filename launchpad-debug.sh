dd814839eb8679290efbf0bbc6a04a01f59ca995
JattMannu <manpreet_firewall@yahoo.com.sg>
2021-04-30 16:10:25 +0800

    Update launchpad-debug.sh

--- launchpad-debug.sh
+++ launchpad-debug.sh
@@ -6,6 +6,10 @@ sudo apt-get install -y x11vnc
 x11vnc -forever -display :0 &
 
 
-sudo ufw allow 8001/udp
-sudo ufw allow 8001/tcp
+sudo ufw allow 5005/udp
+sudo ufw allow 5005/tcp
 sudo ufw allow 5900/tcp
+
+
+
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar bin/current/Launchpad.jar -fs -ClientCommsEnable
